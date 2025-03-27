94ad971dee5da7484e0dc010d5dede91955fe8d9
julialveszz <julia.jesus@sou.inteli.edu.br>
2025-03-27 16:07:20 -0300

    feat: atualização do projeto

--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,81 @@
+const svg = d3.select("#chart");
+const width = +svg.attr("width");
+const height = +svg.attr("height");
 
-const svgWidth = 800;
-const svgHeight = 400;
+const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
 
-const svg = d3.select("body")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight)
-    .append("g")
-    .attr("transform", "translate(50,50)");
+const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-d3.json("scripts/data/dataset.json").then(data => {
-    data.forEach(d => {
-        d.scopeChanges = +d.scopeChanges;
-        d.stressLevel = +d.stressLevel;
-        d.date = new Date(d.date);
+const zoom = d3.zoom()
+    .scaleExtent([0.5, 10]) 
+    .translateExtent([[-100, -100], [width + 100, height + 100]]) 
+    .on("zoom", (event) => {
+        g.attr("transform", event.transform);
     });
 
-    const xScale = d3.scaleTime()
-        .domain(d3.extent(data, d => d.date))
-        .range([0, svgWidth - 100]);
+svg.call(zoom)
+    .call(zoom.transform, d3.zoomIdentity.translate(margin.left, margin.top).scale(1)); 
 
-    const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.stressLevel)])
-        .range([svgHeight - 100, 0]);
+d3.json("scripts/data/dataset.json").then(data => {
+    const x = d3.scalePoint()
+        .domain(data.map(d => d.phase))
+        .range([0, innerWidth])
+        .padding(0.5);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => Math.max(d.scopeChanges, d.stressLevel))])
+        .range([innerHeight, 0]);
+
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    g.append("g")
+        .call(yAxis)
+        .attr("class", "y-axis");
+
+    g.append("g")
+        .call(xAxis)
+        .attr("transform", `translate(0,${innerHeight})`)
+        .attr("class", "x-axis");
 
     const line = d3.line()
-        .x(d => xScale(d.date))
-        .y(d => yScale(d.stressLevel));
+        .x(d => x(d.phase))
+        .y(d => y(d.stressLevel))
+        .curve(d3.curveMonotoneX); 
 
-    svg.append("path")
+    g.append("path")
         .datum(data)
-        .attr("class", "line")
-        .attr("d", line)
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 2)
+        .attr("d", line)
+        .attr("opacity", 0)
         .transition()
-        .duration(2000)
-        .attr("stroke-dasharray", function() {
-            return this.getTotalLength();
-        })
-        .attr("stroke-dashoffset", function() {
-            return this.getTotalLength();
-        })
-        .transition()
-        .duration(2000)
-        .attr("stroke-dashoffset", 0);
+        .duration(1000)
+        .attr("opacity", 1);
 
-    svg.selectAll(".dot")
+    const circles = g.selectAll("circle")
         .data(data)
-        .enter().append("circle")
-        .attr("class", "dot")
-        .attr("cx", d => xScale(d.date))
-        .attr("cy", d => yScale(d.stressLevel))
-        .attr("r", 5)
-        .attr("fill", "orange")
-        .on("mouseover", function(event, d) {
-            d3.select(this).transition()
-                .duration(200)
-                .attr("r", 8);
-            svg.append("text")
-                .attr("x", xScale(d.date))
-                .attr("y", yScale(d.stressLevel) - 10)
-                .attr("class", "tooltip")
-                .text(`Stress Level: ${d.stressLevel}`);
-        })
-        .on("mouseout", function(d) {
-            d3.select(this).transition()
-                .duration(200)
-                .attr("r", 5);
-            svg.selectAll(".tooltip").remove();
+        .enter()
+        .append("circle")
+        .attr("cx", d => x(d.phase))
+        .attr("cy", d => y(d.stressLevel))
+        .attr("r", 8)
+        .attr("fill", d => {
+            if (d.stressLevel === 0 && d.scopeChanges === 0) return "green"; // Onboarding
+            if (d.stressLevel <= 3 && d.scopeChanges <= 2) return "blue"; // Kick-off
+            if (d.stressLevel >= 6 && d.scopeChanges >= 5) return "red"; // Sprint Review 1
+            if (d.stressLevel > 6) return "orange"; // Sprint Review 2
+            if (d.stressLevel <= 5 && d.scopeChanges <= 3) return "purple"; // Sprint Review 3
+            return "gray";
         });
 
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", `translate(0, ${svgHeight - 100})`)
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
-    const zoom = d3.zoom()
-        .scaleExtent([1, 10])
-        .on("zoom", (event) => {
-            svg.attr("transform", event.transform);
-        });
-
-    svg.call(zoom);
+    circles.transition()
+        .duration(1000)
+        .attr("r", 10)
+        .transition()
+        .duration(1000)
+        .attr("r", 8);
 });
